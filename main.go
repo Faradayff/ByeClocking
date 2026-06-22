f689a67edde0620d80735575208bca51723b7ed6
Faradayff <rafa.jerez4@gmail.com>
2026-06-22 20:18:40 +0200

    Improvements proposed by Gemini - Stop signal to stop the application and go-routines by context - Check time to clock in case the day has started already

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
 	"log/slog"
 	"math/rand"
 	"os"
+	"os/signal"
 	"path/filepath"
+	"syscall"
 	"time"
 )
 
@@ -20,7 +23,7 @@ func main() {
 
 	initLogging(*logLevel)
 
-	slog.Debug("Starting application")
+	slog.Info("Starting application")
 
 	cfg, err := LoadConfig("config.json")
 	if err != nil {
@@ -28,47 +31,59 @@ func main() {
 		os.Exit(1)
 	}
 
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
 	for {
-		// TODO: implement summer time
 		slog.Info("Starting the day")
 		clockInTime, lunchTime, lunchFinishTime, clockOutTime := randomizeHours(cfg)
 
-		signalToClock := make(chan bool)
-
-		go waitingTicker(clockInTime, signalToClock)
 		slog.Debug("Waiting to clock in")
+		if toClock, err := waitUntil(ctx, clockInTime); err != nil {
+			break
+		} else if toClock {
+			slog.Info("Clock in time")
+			// clockIn
+		} else {
+			slog.Info("Skipped clock in (missed event)")
+		}
 
-		<-signalToClock
-		slog.Info("Clock in time")
-
-		// clockIn
-
-		go waitingTicker(lunchTime, signalToClock)
 		slog.Debug("Waiting to go to lunch")
+		if toClock, err := waitUntil(ctx, lunchTime); err != nil {
+			break
+		} else if toClock {
+			slog.Info("Lunch time")
+			// clockPause
+		} else {
+			slog.Info("Skipped lunch time (missed event)")
+		}
 
-		<-signalToClock
-		slog.Info("Lunch time")
-
-		// clockPause
-
-		go waitingTicker(lunchFinishTime, signalToClock)
 		slog.Debug("Waiting to go back from lunch")
+		if toClock, err := waitUntil(ctx, lunchFinishTime); err != nil {
+			break
+		} else if toClock {
+			slog.Info("Back from lunch time")
+			// clockResume
+		} else {
+			slog.Info("Skipped back from lunch time (missed event)")
+		}
 
-		<-signalToClock
-		slog.Info("Back from lunch time")
-
-		// clockResume
-
-		go waitingTicker(clockOutTime, signalToClock)
 		slog.Debug("Waiting to clock out")
+		if toClock, err := waitUntil(ctx, clockOutTime); err != nil {
+			break
+		} else if toClock {
+			slog.Info("Clock out time")
+			// clockOut
+		} else {
+			slog.Info("Skipped clock out (missed event)")
+		}
 
-		<-signalToClock
-		slog.Info("Clock out time")
-
-		// clockOut
-
-		waitUntilTomorrow(cfg.ClockIn.Time)
+		if err := waitUntilTomorrow(ctx, cfg.ClockIn.Time); err != nil {
+			break
+		}
 	}
+
+	slog.Info("Application shut down gracefully")
 }
 
 func initLogging(loglevel string) {
@@ -99,19 +114,51 @@ func initLogging(loglevel string) {
 	slog.SetDefault(logger)
 }
 
-func waitingTicker(targetHour time.Time, signal chan<- bool) {
+// waitUntil waits until the targetHour. Returns true if the event should be executed,
+// false if it was skipped due to being in the past, or an error if context was canceled.
+func waitUntil(ctx context.Context, targetHour time.Time) (bool, error) {
 	timeToClock := time.Until(targetHour)
-	slog.Debug("Waiting Ticker is going to wait", "duration", timeToClock.Minutes())
-	<-time.After(timeToClock)
-	signal <- true
+
+	// If the event is more than 5 minutes in the past, consider it missed and skip it.
+	if timeToClock < -5*time.Minute {
+		slog.Debug("Time to clock was way before, skipping it", "timeToClock", timeToClock.Round(time.Minute))
+		return false, nil
+	} else if timeToClock <= 0 { // If it's slightly in the past (e.g., up to 5 mins), execute immediately
+		slog.Debug("Time to clock was just a moment ago, time to clock", "timeToClock", timeToClock.Round(time.Minute))
+		return true, nil
+	}
+
+	slog.Debug("Waiting until time", "duration", timeToClock.Round(time.Minute))
+	timer := time.NewTimer(timeToClock)
+	defer timer.Stop()
+
+	select {
+	case <-timer.C:
+		return true, nil
+	case <-ctx.Done():
+		return false, ctx.Err()
+	}
 }
 
-func waitUntilTomorrow(clockIn time.Time) {
+func waitUntilTomorrow(ctx context.Context, clockIn time.Time) error {
 	now := time.Now()
 	tomorrow := time.Date(now.Year(), now.Month(), now.Day()+1, clockIn.Hour(), clockIn.Minute(), clockIn.Second(), clockIn.Nanosecond(), now.Location())
 	wakeUpTime := time.Until(tomorrow) - time.Hour
-	slog.Debug("Waiting until tomorrow", "duration", wakeUpTime.Minutes())
-	<-time.After(wakeUpTime)
+	slog.Debug("Waiting until tomorrow", "duration", wakeUpTime.Round(time.Minute))
+
+	if wakeUpTime <= 0 {
+		return nil
+	}
+
+	timer := time.NewTimer(wakeUpTime)
+	defer timer.Stop()
+
+	select {
+	case <-timer.C:
+		return nil
+	case <-ctx.Done():
+		return ctx.Err()
+	}
 }
 
 func randomizeHours(cfg *Config) (time.Time, time.Time, time.Time, time.Time) {
@@ -142,6 +189,6 @@ func initDelays(cfg *Config) (clockInDelay time.Duration, lunchDelay time.Durati
 		lunchDuration = time.Duration(cfg.MinTimeToLunch + rand.Intn(cfg.MaxTimeToLunch-cfg.MinTimeToLunch+1))
 	}
 
-	slog.Debug("Delays initialized", "ClockInDelay", clockInDelay, "lunchDelay", lunchDelay, "lunchDuration", lunchDuration, "clockOutDelay", clockOutDelay)
+	slog.Debug("Delays initialized", "ClockInDelay", clockInDelay.Round(time.Minute), "lunchDelay", lunchDelay.Round(time.Minute), "lunchDuration", lunchDuration.Round(time.Minute), "clockOutDelay", clockOutDelay.Round(time.Minute))
 	return
 }
